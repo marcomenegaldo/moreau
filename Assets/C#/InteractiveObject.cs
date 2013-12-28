ea8c30de471d7897e646c9a98a68a46858a3c2d5
Giacomo Cappellini <g.cappellini@mixelweb.it>
2013-12-28 21:22:17 +0100

    Aggiunto progetto Unity Aggiunta scena TestPathScene: questa scena è rivolta a sperimentare la tecnologia di navigation mesh per la costruzione di un terreno camminabile 3D o 2.5D capace di gestire diversi livelli di profondità e percorsi ad anello sia in 2D che in 3D - in questo progetto puoi cliccare ovunque nello schermo e la sfera cercherà di raggiungere la posizione camminando sul terreno fornito - muovendo il mouse su uno dei cubi (Oggetti Interattivi) apparirà una breve descrizione posizionata in base alla posizione del cursore - il cursore cambierà colore quando sarà sovrapposto ad un oggetto interattivi

---
+++ Assets/C#/InteractiveObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveObject : MonoBehaviour {
+
+	public string hintName;
+	public string hintDescripton;
+
+	void OnMouseEnter() {
+		Cursor.SetCursor( InputHandler.instance.hintCursor, InputHandler.instance.cursorHotSpot, InputHandler.instance.cursorMode );
+
+		InputHandler.instance.interactiveObjectHint.enabled = true;
+		InputHandler.instance.interactiveObjectHint.text = string.Format( "<b>{0}</b>\n{1}", hintName, hintDescripton );
+	}
+
+	void OnMouseExit() {
+		Cursor.SetCursor( InputHandler.instance.normalCursor, InputHandler.instance.cursorHotSpot, InputHandler.instance.cursorMode );
+
+		InputHandler.instance.interactiveObjectHint.enabled = false;
+	}
+
+	void OnMouseOver() {
+		Vector3 mouseViewportPoint = Camera.main.ScreenToViewportPoint( Input.mousePosition );
+
+		InputHandler.instance.interactiveObjectHint.anchor = mouseViewportPoint.x > 0.5f ? TextAnchor.UpperRight : TextAnchor.UpperLeft;
+
+		InputHandler.instance.interactiveObjectHint.transform.position = mouseViewportPoint + (Vector3)( InputHandler.instance.interactiveObjectHintViewportOffset * ( mouseViewportPoint.x > 0.5f ? -1.0f : 1.0f ) );
+	}
+
+}
